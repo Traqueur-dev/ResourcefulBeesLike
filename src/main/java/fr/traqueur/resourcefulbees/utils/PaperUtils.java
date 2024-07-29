47c43ccb530e5f7a1e56311f8427dc7075a355d7
Traqueur-dev <traqueur.dev@gmail.com>
2024-07-29 13:53:31 +0200

    fix (MessageUtils) : fix paper version of MessageUtils

--- src/main/java/fr/traqueur/resourcefulbees/utils/PaperUtils.java
+++ src/main/java/fr/traqueur/resourcefulbees/utils/PaperUtils.java
@@ -1,32 +1,31 @@
 package fr.traqueur.resourcefulbees.utils;
 
 import fr.traqueur.resourcefulbees.api.utils.MessageUtils;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
 public class PaperUtils implements MessageUtils {
 
-//    public static Component of(String text) {
-//        return Component.text(text).decoration(TextDecoration.ITALIC, false);
-//    }
-
     @Override
     public void sendMessage(Player player, String message) {
-        player.sendMessage(message);
+        player.sendMessage(Component.text(message));
     }
 
     @Override
     public void success(Player player, String s) {
-        this.sendMessage(player, ChatColor.translateAlternateColorCodes('&', "&a" + s));
+        player.sendMessage(Component.text(s, NamedTextColor.GREEN));
     }
 
     @Override
     public void error(Player player, String s) {
-        this.sendMessage(player, ChatColor.translateAlternateColorCodes('&', "&c" + s));
+        player.sendMessage(Component.text(s, NamedTextColor.RED));
     }
 
     @Override
     public String reset(String s) {
-        return ChatColor.translateAlternateColorCodes('&', "&r" + s);
+        return Component.text(s).decoration(TextDecoration.ITALIC, false).toString();
     }
 }
